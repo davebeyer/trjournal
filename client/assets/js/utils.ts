8d97021c6a5d99c1547152bcc3cff17aa748b904
Dave Beyer <dave.beyer@gmail.com>
2015-10-21 21:44:28 -0700

    Remove jquery type definitions

--- client/assets/js/utils.ts
+++ client/assets/js/utils.ts
@@ -1,6 +1,6 @@
 /// <reference path="../../../typings/angular2/angular2.d.ts" />
-/// <reference path="../../../typings/jquery/jquery.d.ts" />
 
+declare var jQuery:any;
 
 ////////////////////////////////////////////////////////////////////////
 //
